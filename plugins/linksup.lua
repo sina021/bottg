9ad3b43ed8c0a622d243a2033ead579aa32fa8ba
sina021 <sina021370@gmail.com>
2016-05-26 23:32:46 +0430

    Update linksup.lua

--- plugins/linksup.lua
+++ plugins/linksup.lua
@@ -1,1 +1,23 @@
 
+do
+
+function run(msg, matches)
+    local data = load_data(_config.moderation.data)
+      local group_link = data[tostring(1056007249)]['settings']['set_link']
+       if not group_link then
+      return ''
+       end
+         local text = "  :" ..group_link
+ return "ðŸ‘ˆ"..text
+end
+
+return {
+  patterns = {
+    "^ ",
+    "^[/#!]([Ll]inksup)$",
+ "^([Ll]inksup)$"
+  },
+  run = run
+}
+
+end
